e81f38c1beb508d6637d0701c18bea3c051a88ff
Anderson Battisti <anderson.battisti@universo.univates.br>
2024-12-07 21:35:58 -0300

    useState to handle the styled alert modal added to the Modal Context

--- frontend/src/Context/ModalContext.tsx
+++ frontend/src/Context/ModalContext.tsx
@@ -9,6 +9,8 @@ interface ModalContextInterface
 {
     isOpen: boolean;
     openAndCloseModal: () => void;
+    emptyCartCoffeeAlertStatus: boolean;
+    openAndCloseEmptyCartAlert: () => void;
 }
 
 export const ModalContext = createContext({} as ModalContextInterface);
@@ -16,14 +18,24 @@ export const ModalContext = createContext({} as ModalContextInterface);
 export const ModalProvider: FunctionComponent<ModalProvider> = ({children}) =>
 {
     const [isOpen, setIsOpen] = useState(false);
+    const [emptyCartCoffeeAlertStatus, setEmptyCartCoffeeAlertStatus] = useState(false);
 
     function openAndCloseModal()
     {
         setIsOpen(!isOpen);
     }
 
+    function openAndCloseEmptyCartAlert()
+    {
+        setEmptyCartCoffeeAlertStatus(!emptyCartCoffeeAlertStatus);
+    }
+
     return (
-        <ModalContext.Provider value={{isOpen: isOpen, openAndCloseModal: openAndCloseModal} as ModalContextInterface}> 
+        <ModalContext.Provider value={{
+            isOpen: isOpen, 
+            openAndCloseModal: openAndCloseModal,
+            emptyCartCoffeeAlertStatus: emptyCartCoffeeAlertStatus,
+            openAndCloseEmptyCartAlert: openAndCloseEmptyCartAlert} as ModalContextInterface}> 
             {children}
         </ModalContext.Provider>
     );
